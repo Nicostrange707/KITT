507e3e5458bf4bace64302084b4ef2bc66459d74
Nicostrange707 <74682984+Nicostrange707@users.noreply.github.com>
2022-09-21 19:32:01 +0200

    Add files via upload

---
+++ KITT2.py
@@ -0,0 +1,439 @@
+from time import sleep
+import pyglet
+from art import *
+import os
+
+#Name wird im der Console ausgegeben
+os.system("cls")
+sleep(2)
+os.system("start KITT.gif")
+tprint("KITT")
+
+#Intro
+music = pyglet.resource.media('sound/Intro.mp3')
+music.play()
+sleep(72)
+
+os.system("start KITT_Interface.gif")
+
+#Erster Sprachebefehl
+music2=pyglet.resource.media('sound/aber_zu_welchem_zweck.mp3')
+music2.play()
+sleep(2)
+
+#Zweiter Sprachbefehl
+music3=pyglet.resource.media('sound/absolut_lecherlich.mp3')
+music3.play()
+sleep(2)
+
+#Dritter Sprachbefehl
+music4=pyglet.resource.media('sound/bitte_lassen_sie_mich_ihnen_helfen.mp3')
+music4.play()
+sleep(2)
+
+#Vierter Sprachbefehl
+music5=pyglet.resource.media('sound/bitte_vertrauen_sie_mir.mp3')
+music5.play()
+sleep(2)
+
+#Fünfter Sprachbefehl
+music6=pyglet.resource.media('sound/computer_sind_sehr_eigen.mp3')
+music6.play()
+sleep(2)
+
+#Sechster Sprachbefehl
+music7=pyglet.resource.media('sound/danke.mp3')
+music7.play()
+sleep(1)
+
+#Siebter Sprachbefehl
+music8=pyglet.resource.media('sound/darf_ich_vorschlagen_das_sie_das_auto_auf_die_automatische_steuerung_umschalten.mp3')
+music8.play()
+sleep(5)
+
+#Achter Sprachbefehl
+music9=pyglet.resource.media('sound/das_ist_ein_teil_meiner_arbeit.mp3')
+music9.play()
+sleep(2)
+
+#Neunter Sprachbefehl
+music10=pyglet.resource.media('sound/das_kann_leicht_täuschen.mp3')
+music10.play()
+sleep(3)
+
+#Zenter Sprachbefehl
+music11=pyglet.resource.media('sound/das_tröstet_mich.mp3')
+music11.play()
+sleep(2)
+
+#Elfter Sprachebefehl
+music12=pyglet.resource.media('sound/das_überrascht_mich_nicht.mp3')
+music12.play()
+sleep(1)
+
+#Zwölfter Sprachbefehl
+music13=pyglet.resource.media('sound/das_würden_sie_doch_nicht_tun.mp3')
+music13.play()
+sleep(2)
+
+#Dreizenter Sprachbefehel
+music14=pyglet.resource.media('sound/der_war_ja_ganz_schön_gerissen.mp3')
+music14.play()
+sleep(2)
+
+#Vierzenter Sprachbefehl
+music15=pyglet.resource.media('sound/dies_ist_arbeit_kein_vergnügen.mp3')
+music15.play()
+sleep(2)
+
+#Fünftzenter Sprachbefehl
+music16=pyglet.resource.media('sound/ein_sowieso_wertloser_zeitvertreib.mp3')
+music16.play()
+sleep(2)
+
+#Sechzenter Sprachbefehl
+music17=pyglet.resource.media('sound/es_gibt_keinen_grund_für_erhühte_lautstärke.mp3')
+music17.play()
+sleep(3)
+
+#Siebzenter Sprachbefehl
+music18=pyglet.resource.media('sound/es_war_ihre_ideee_zu_spielen.mp3')
+music18.play()
+sleep(2)
+
+#Achzenter Sprachbefehl
+music19=pyglet.resource.media('sound/etwas_mehr_rücksichtnahme_hätte_ich_schon_verdient.mp3')
+music19.play()
+sleep(3)
+
+#Neunzenter Sprachbefehl
+music20=pyglet.resource.media('sound/ganz_wie_sie_wünschen.mp3')
+music20.play()
+sleep(2)
+
+#Zwanzigster Sprachbefehl
+music21=pyglet.resource.media('sound/gestatten_sie_das_ich_mich_wieder_einschalte.mp3')
+music21.play()
+sleep(2)
+
+#Einundzwanzigster Sprachbefehl
+music22=pyglet.resource.media('sound/glauben_sie_nicht_das_sie_mich_für_den_fall_das_sie_nicht_wiederkommen_vorprogrammieren_sollten.mp3')
+music22.play()
+sleep(4)
+
+#Zweiundzwanzigster Sprachbefel
+music23=pyglet.resource.media('sound/gut.mp3')
+music23.play()
+sleep(1)
+
+#Dreiundzwanzigster Sprachbefehl
+music24=pyglet.resource.media('sound/gute_nacht.mp3')
+music24.play()
+sleep(1)
+
+#Vierundzwanzigster Sprachbefehl
+music25=pyglet.resource.media('sound/haben_sie_mich_wirklich_vergessen.mp3')
+music25.play()
+sleep(2)
+
+#Fünfundzwanzigster Sprachbefehl
+music26=pyglet.resource.media('sound/halten_die_sie_gefangen.mp3')
+music26.play()
+sleep(2)
+
+#Sechsundzwanzigster Sprachbefehl
+music27=pyglet.resource.media('sound/ich_bin_der_knight_industrie_2000.mp3')
+music27.play()
+sleep(3)
+
+#Siebenundzwanzigster Sprachbefehl
+music28=pyglet.resource.media('sound/ich_bin_die_stimme_des_microprozessors_des_modells2000_von_knight_industries_k_i_t_t_für_den_einfachen_sprachgebrauch_oder_kitt_wenn_sie_das_vorziehen.mp3')
+music28.play()
+sleep(9)
+
+#Achundzwanzigster Sprachbefehl
+music29=pyglet.resource.media('sound/ich_bin_gerne_bereit_ihre_fragen_zu_beantworten.mp3')
+music29.play()
+sleep(3)
+
+#Neunundzwanzigster Sprachbefehl
+music30=pyglet.resource.media('sound/ich_bin_nicht_okay.mp3')
+music30.play()
+sleep(2)
+
+#Dreisigster Sprachbefehl
+music31=pyglet.resource.media('sound/ich_bin_nur_ein_schatten_meiner_selbst.mp3')
+music31.play()
+sleep(3)
+
+#Einunddreisigster Sprachbefehl
+music32=pyglet.resource.media('sound/ich_bin_programmiert_auf_ihre_bedürfnisse_einzugehen.mp3')
+music32.play()
+sleep(3)
+
+#Zweiunddreisigster Sprachbefehl
+music33=pyglet.resource.media('sound/ich_bin_schon_an_der_arbeit.mp3')
+music33.play()
+sleep(2)
+
+#Zweiunddreisigster Sprachbefehl
+music34=pyglet.resource.media('sound/ich_bin_tragbar.mp3')
+music34.play()
+sleep(2)
+
+#Dreiunddreisigster Sprachbefehl
+music35=pyglet.resource.media('sound/ich_denke_schon.mp3')
+music35.play()
+sleep(2)
+
+#Vierunddreisigster Sprachbefehl
+music36=pyglet.resource.media('sound/ich_finde_wir_sind_sehr_langsam.mp3')
+music36.play()
+sleep(3)
+
+#Fünfunddreisigster Sprachbefehl
+music37=pyglet.resource.media('sound/ich_fühle_mich_wie_der_hauptpreis_in_einer_tombola.mp3')
+music37.play()
+sleep(3)
+
+#Sechsunddreisigster Sprachbefehl
+music38=pyglet.resource.media('sound/ich_glaube_das_ein_betrechtlicher_riesikofaktor_mit_diesem_einsatz_verbunden_ist.mp3')
+music38.play()
+sleep(3)
+
+#Siebenunddreisigster Sprachbefehl
+music39=pyglet.resource.media('sound/ich_habe_nicht_vor_auch_nur_einen_reifen_in_ein_flugzeug_zu_setzen.mp3')
+music39.play()
+sleep(4)
+
+#Achtunddreisigster Sprachbefehl
+music40=pyglet.resource.media('sound/ich_habe_schwierigkeiten_sie_zu_empfangen.mp3')
+music40.play()
+sleep(3)
+
+#Neununddreisigster Sprachbefehl
+music41=pyglet.resource.media('sound/ich_habe_sie_nur_überwacht.mp3')
+music41.play()
+sleep(2)
+
+#Vierzigster Sprachbefehl
+music42=pyglet.resource.media('sound/ich_verschaffe_mir_zugang_zu_allem_was_nicht_codiert_ist.mp3')
+music42.play()
+sleep(3)
+
+#Einundvierzigster Sprachbefehl
+music43=pyglet.resource.media('sound/ich_verstehe.mp3')
+music43.play()
+sleep(2)
+
+#Zweiundvierzigster Sprachbefehl
+music44=pyglet.resource.media('sound/ich_verstehe_garnichts.mp3')
+music44.play()
+sleep(2)
+
+#Dreiundvierzigster Sprachbefehl
+music45=pyglet.resource.media('sound/ich_weiß.mp3')
+music45.play()
+sleep(1)
+
+#Vierundvierzigster Sprachbefehl
+music46=pyglet.resource.media('sound/ich_weiß_es_ist_lecherlich_mit_einem_fernsehr_zu_reden_aber_irgendwas_könnten_sie_schon_sagen.mp3')
+music46.play()
+sleep(5)
+
+#Fünfundvierzigster Sprachbefehl
+music47=pyglet.resource.media('sound/ja_das_wäre_schwierig.mp3')
+music47.play()
+sleep(2)
+
+#Sechsundvierzigster Sprachbefehl
+music48=pyglet.resource.media('sound/kein_problem.mp3')
+music48.play()
+sleep(1)
+
+#Siebenundvierzigster Sprachbefehl
+music49=pyglet.resource.media('sound/leider_bin_ich_nicht_dadrauf_programmiert_ihre_wünsche_zu_übergehen.mp3')
+music49.play()
+sleep(4)
+
+#Achtundvierzigster Sprachbefehl
+music50=pyglet.resource.media('sound/meine_gegenwärtige_behausung_zeigt_auch_nicht_mein_wahres_ich.mp3')
+music50.play()
+sleep(4)
+
+#Neunundvierzigster Sprachbefehl
+music51=pyglet.resource.media('sound/meine_sensoren_orten_das_die_unbewaffnet_sind.mp3')
+music51.play()
+sleep(3)
+
+#Fünfzigster Sprachbefehl
+music52=pyglet.resource.media('sound/meinen_sie_das_ernst.mp3')
+music52.play()
+sleep(2)
+
+#Einundfünfzigster Sprachbefehl
+music53=pyglet.resource.media('sound/Mister_Knight.mp3')
+music53.play()
+sleep(1)
+
+#Zweiundfünfzigster Sprachbefehl
+music54=pyglet.resource.media('sound/natürlich_wird_es_das.mp3')
+music54.play()
+sleep(2)
+
+#Dreiundfünfzigster Sprachbefehl
+music55=pyglet.resource.media('sound/nicht_ein_wort.mp3')
+music55.play()
+sleep(2)
+
+#Vierundfünfzigster Sprachbefehl
+music56=pyglet.resource.media('sound/nun_stehen_sie_nicht_so_sinnlos_herrum.mp3')
+music56.play()
+sleep(3)
+
+#Fünfundfünfzigster Sprachbefehl
+music57=pyglet.resource.media('sound/sagen_sie_nichts.mp3')
+music57.play()
+sleep(2)
+
+#Sechsundfünfzigster Sprachbefehl
+music58=pyglet.resource.media('sound/schließlich_bin_ich_so_programmiert_worden_nicht_war.mp3')
+music58.play()
+sleep(3)
+
+#Siebenundfünfzigster Sprachbefehl
+music59=pyglet.resource.media('sound/schließlich_sind_wir_ja_auch_nur_menschen.mp3')
+music59.play()
+sleep(3)
+
+#Achtundfünfzigster Sprachbefehl
+music60=pyglet.resource.media('sound/schon_besser.mp3')
+music60.play()
+sleep(2)
+
+#Neunundfünfzigster Sprachbefehl
+music61=pyglet.resource.media('sound/sehr_witzig.mp3')
+music61.play()
+sleep(2)
+
+#Sechzigster Sprachbefehl
+music62=pyglet.resource.media('sound/sein_sie_bitte_äuserst_forsichtig.mp3')
+music62.play()
+sleep(2)
+
+#Einundsechzigster Sprachbefehl
+music63=pyglet.resource.media('sound/selbstverständlich.mp3')
+music63.play()
+sleep(2)
+
+#Zweiundsechzigster Sprachbefehl
+music64=pyglet.resource.media('sound/sie_nannten_mich_immer_kitt.mp3')
+music64.play()
+sleep(2)
+
+#Dreiundsechzigster Sprachbefehl
+music65=pyglet.resource.media('sound/sind_sie_da_ganz_sicher.mp3')
+music65.play()
+sleep(2)
+
+#Vierundsechzigster Sprachbefehl
+music66=pyglet.resource.media('sound/sind_sie_in_ordnung.mp3')
+music66.play()
+sleep(2)
+
+#Fünfundsechzigster Sprachbefehl
+music67=pyglet.resource.media('sound/so_hatte_ich_mir_das_wiedersehen_nicht_vorgestellt.mp3')
+music67.play()
+sleep(3)
+
+#Sechsundsechzigster Sprachbefehl
+music68=pyglet.resource.media('sound/so_ja.mp3')
+music68.play()
+sleep(1)
+
+#Siebenundsechzigster Sprachbefehl
+music69=pyglet.resource.media('sound/sollten_wir_das_wirklich_tun.mp3')
+music69.play()
+sleep(2)
+
+#Achtundsechzigster Sprachbefehl
+music70=pyglet.resource.media('sound/stattdessen_könnten_sie_mich_über_unseren_auftrag_in_kenntnis_setzen.mp3')
+music70.play()
+sleep(3)
+
+#Neunundsechzigster Sprachbefehl
+music71=pyglet.resource.media('sound/war_nur_so_ein_gedanke.mp3')
+music71.play()
+sleep(2)
+
+#Siebzigster Sprachbefehl
+music72=pyglet.resource.media('sound/warum_können_die_menschen_nicht_mit_dem_zu_frieden_sein_was_sie_haben.mp3')
+music72.play()
+sleep(3)
+
+#Einundsiebzigster Sprachbefehl
+music73=pyglet.resource.media('sound/was_ist_urlaub.mp3')
+music73.play()
+sleep(2)
+
+#Zweiundsiebzigster Sprachbefehl
+music74=pyglet.resource.media('sound/was_möchten_sie_gerne_hören.mp3')
+music74.play()
+sleep(2)
+
+#Dreiundsiebzigster Sprachbefehl
+music75=pyglet.resource.media('sound/was_soll_ich_tun_während_sie_weg_sind.mp3')
+music75.play()
+sleep(2)
+
+#Vierundsiebzigster Sprachbefehl
+music76=pyglet.resource.media('sound/welche_art_von_ablenkung_wäre_den_passend.mp3')
+music76.play()
+sleep(3)
+
+#Fünfundsiebzigster Sprachbefehl
+music77=pyglet.resource.media('sound/wenn_autos_zum_fliegen_bestimmt_wären_hätten_sie_flügel.mp3')
+music77.play()
+sleep(3)
+
+#Sechsundsiebzigster Sprachbefehl
+music78=pyglet.resource.media('sound/wenn_ich_nicht_wäre_müssten_sie_zu_fuß_gehen.mp3')
+music78.play()
+sleep(3)
+
+#Siebenundsiebzigster Sprachbefehl
+music79=pyglet.resource.media('sound/wenn_meine_laserkannone_in_die_falschen_hände_gerätt_kann_sie_verherrende_schäden_anrichten.mp3')
+music79.play()
+sleep(5)
+
+#Achtundsiebzigster Sprachbefehl
+music80=pyglet.resource.media('sound/wie_sie_schon_angedeutet_haben_bin_ich_nur_ein_auto.mp3')
+music80.play()
+sleep(3)
+
+#Neunundsiebzigster Sprachbefehl
+music81=pyglet.resource.media('sound/wie_sie_unschwer_sehen_können_bin_ich_in_einen_fernseher_eingesperrt.mp3')
+music81.play()
+sleep(4)
+
+#Achzigster Sprachbefehl
+music82=pyglet.resource.media('sound/wie_sie_wünschen_mister_knight.mp3')
+music82.play()
+sleep(2)
+
+#Einundachzigster Sprachbefehl
+music83=pyglet.resource.media('sound/wir_sind_partner_sie_und_ich_wir_sind_ein_team.mp3')
+music83.play()
+sleep(4)
+
+#Zweiundachzigster Sprachbefehl
+music84=pyglet.resource.media('sound/wollen_sie_das_ich_komme_und_sie_raushole.mp3')
+music84.play()
+sleep(2)
+
+#Dreiundachzigster Sprachbefehl
+music85=pyglet.resource.media('sound/würden_sie_es_vllt_mit_mir_teilen.mp3')
+music85.play()
+sleep(2)
+
+pyglet.app.run()
